77fdc540d6fe1ad35802470737bc455b61d9a36f
Balchandar reddy <sangambalchanderreddy@Sangams-MacBook-Air.local>
2021-01-07 16:07:24 +0530

    Circumcenter finding added

--- example.cpp
+++ example.cpp
@@ -2,6 +2,7 @@
 #include "Delaunay.hpp"
 #include <vector>
 #include <stdio.h>
+#include <math.h>
 
 #include <string>
 #include <sstream>
@@ -14,14 +15,56 @@
 // mpic++ -std=c++11 example.cpp -lmetis
 // mpirun -np 4 ./a.out
 
+
 using namespace std;
 
 // Test Data
 ifstream inFile ("input.txt");
+vector<vector<int>> points
+    {
+    {1,2},{2,3},{-4,6},{-2,3},{4,3},{-10,3},{4,8},
+    {5,-3},{-1,7},{0,5},{4,4},{5,-1}
+    };
 
 // Vertices XS - x coordinates & YS - y coordinates
 vector<int> XS;
 vector<int> YS;
+
+float threshold1 = 2;
+float threshold2 = 5;
+
+float* circumcenter(float Px, float Py, float Qx, float Qy, float Rx, float Ry
+                    ,float x, float y, float z)
+{
+  float* arr = new float[2];
+  cout << x << " " << y << " " << z << endl;
+  float angleA = acos((-x*x+y*y+z*z)/(2*y*z));
+  float angleB = acos((x*x-y*y+z*z)/(2*x*z));
+  float angleC = acos((x*x+y*y-z*z)/(2*x*y));
+
+  cout << angleA << " " << angleB << " " << angleC << endl;
+
+  arr[0] = (Px * sin(2*angleA) + Qx*sin(2*angleB) + Rx*sin(2*angleC))/
+           (sin(2*angleA) + sin(2*angleB) + sin(2*angleC));
+  arr[1] = (Py * sin(2*angleA) + Qy*sin(2*angleB) + Ry*sin(2*angleC))/
+           (sin(2*angleA) + sin(2*angleB) + sin(2*angleC));
+           
+  cout << arr[0] << " " << arr[1] << endl;
+  return arr;
+}
+
+float distance(int x1, int y1, int x2, int y2) 
+{ 
+    // Calculating distance 
+    return sqrt(pow(x2 - x1, 2) +  
+                pow(y2 - y1, 2) * 1.0); 
+} 
+
+float triangle_area(float a, float b, float c)
+{
+  float p = (a+b+c)/2;
+  return sqrt(p*(p-a)*(p-b)*(p-c));
+}
 
 int main(int argc, char** argv)
 {
@@ -120,15 +163,17 @@ int main(int argc, char** argv)
     int ret2 = METIS_PartMeshNodal( 
       &nElements, &nVertices, eptr.data(), eind.data(), NULL, NULL,
       &nParts, NULL, NULL, &objval, epart.data(), npart.data());
+      
+    if(process_Rank == 0){
+      cout << endl <<"----------Elements Partition-------" << endl;
+      for(unsigned part_i = 0; part_i < epart.size(); part_i++){
+        std::cout << "Element " << part_i << " Allotted to the P" << epart[part_i] << std::endl;
+      }
 
-    for(unsigned part_i = 0; part_i < epart.size(); part_i++){
-      cout << "EPart" << process_Rank << ":";
-	    std::cout << part_i << " " << epart[part_i] << std::endl;
-    }
-
-    for(unsigned part_i = 0; part_i < npart.size(); part_i++){
-      cout << process_Rank << ":";
-	    std::cout << part_i << " " << npart[part_i] << std::endl;
+      cout << endl << "----------Nodes Partition----------" << endl;
+      for(unsigned part_i = 0; part_i < npart.size(); part_i++){
+        std::cout << "Node " << part_i << " Allotted to P" << npart[part_i] << std::endl;
+      }
     }
 
 
@@ -147,6 +192,71 @@ int main(int argc, char** argv)
           partElements.push_back(part_i);
         }
       }
+
+      // cout << partNodes[0];cout << partNodes[1];cout << partNodes[2];
+      cout << partNodes.size();
+      for(int i=0;i<partNodes.size();i++) {
+        //vertices of that triangle
+        int Px = points[eind[i*3]][0];
+        int Py = points[eind[i*3]][1];
+        int Qx = points[eind[i*3+1]][0];
+        int Qy = points[eind[i*3+1]][1];
+        int Rx = points[eind[i*3+2]][0];
+        int Ry = points[eind[i*3+2]][1];
+
+        float x = distance(Qx,Qy,Rx,Ry);
+        float y = distance(Rx,Ry,Px,Py);
+        float z = distance(Px,Py,Qx,Qy);
+
+        float shortest = x < y ? (x < z ? x : z) : (y < z ? y : z);
+        float area = triangle_area(x,y,z);
+        float circumRadius = x*y*z/(4*area);
+
+        if(circumRadius/shortest > threshold1 || area > threshold2) {
+          //bad triangle
+          //find the circumcenter
+          cout << "IN" << endl;
+          cout << "Shortest" << shortest << endl;
+          cout << "Circumradius" << circumRadius << endl;
+          cout << "Circum/shortest" << circumRadius/shortest << endl; 
+          cout << "Area" << area << endl;
+
+          //circumcenter co-ordinates
+
+          float* ptr = circumcenter(Px,Py,Qx,Qy,Rx,Ry,x,y,z);
+
+          cout << Px << " " << Py << " " << Qx << " " << Qy 
+                << " " << Rx << " " << Ry << endl;
+
+          cout << ptr[0] << " " << ptr[1] << endl;
+          //got circumcenter
+
+          //find shared edges of this process with other processes
+          //take common edges and check whether the circumcenter encroaches the segment
+
+          //if true
+          //send split messages
+          //local cavity
+
+          //else
+          //local cavity
+
+        }
+
+        // for(int j=0;j<points.size();j++) {
+        //   //iterate through all the points with robustpredicates
+        //   if(predicates::incircle(Px,Py, Qx,Qy, Rx,Ry, points[j][0],points[j][1])<0) {
+        //     //point lies inside the circle
+        //     //capture the point
+        //     cout << "Inside";
+        //     cout << j;
+        //     //send a split request, if the point belongs to other process
+            
+        //   }
+        // }
+        cout << endl;
+
+      }
     }
 
     else if(process_Rank == 1){
@@ -161,7 +271,11 @@ int main(int argc, char** argv)
           partElements.push_back(part_i);
         }
       }
+      // cout << partNodes[0];cout << partNodes[1];cout << partNodes[2];
+      cout << partNodes.size();
+
     }
+
 
     MPI_Finalize();
 
